a47dd9f3abd1bfb6f6596cbfd3f73357b77c55a1
diegogit2 <diegojeftha18@gmail.com>
2023-04-20 17:15:15 +0200

    completed IWA15 challenge1

---
+++ JavaScript/scripts.js
@@ -0,0 +1,49 @@
+// scripts.js
+
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]], 
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below [10, 12, 14, 5, 7, 13, 11, 15, 4, 8, 6, 2, 1, 3, 9]
+
+const first = data.lists[0][1]
+const second = data.lists[1][1]
+const third = data.lists[2][1]
+
+
+const result = []
+
+const extractBiggest = () => {
+	if (first[first.length -1] > second[second.length -1]) {
+		return first.pop()
+	} else {
+        return second.pop() || third.pop()
+    }	
+}
+
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)
